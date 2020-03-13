bf49cd1c4e58cc3ae7328f78eacd8d9a259b8758
Max <maxlambert32@ymail.com>
2020-03-12 22:21:01 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,20 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup
 
-url = input("Please enter website. \n")
 
-uClient = uReq(url)
+
+uClient = uReq("https://lol.gamepedia.com/LCS/2020_Season/Spring_Season/Scoreboards/Week_7")
 page_html = uClient.read()
 uClient.close()
 
-page_soup = soup(page_html, "html.parser")
+page_soup = BeautifulSoup(page_html, "html.parser")
+
+
+list = page_soup.findAll("div", attrs={"class":"inline-content"})
+
+for table in list:
+    tableGold = table.find_all("div", {"class": "sb-p-stat sb-p-stat-gold"})
+    tableName = table.find_all("div", {"class": "sb-p-name"})
+    print(tableGold.text)
+    print(tableName.text)
 
