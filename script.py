60bdeed48d06f1d6b0cf6bf93d630b9e9444a715
Max <maxlambert32@ymail.com>
2020-03-12 20:56:06 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,11 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup
 
-print("hello")
+url = input("Please enter website. \n")
+
+uClient = uReq(url)
+page_html = uClient.read()
+uClient.close()
+
+page_soup = soup(page_html, "html.parser")
+
