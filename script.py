f45ceeed6218e1d5c49e7440a4e99a9cce9eea45
Max <maxlambert32@ymail.com>
2020-03-13 00:50:09 -0400

    dwd

--- script.py
+++ script.py
@@ -1,26 +1,48 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup
-import xlwt
-from xlwt import Workbook
+from openpyxl import Workbook
+import pandas as pd
+
 
 wb = Workbook()
-sheet1 = wb.add_sheet('Sheet 1')
+ws = wb.active
+
+
 uClient = uReq("https://lol.gamepedia.com/LCS/2020_Season/Spring_Season/Scoreboards/Week_7")
 page_html = uClient.read()
 uClient.close()
 
 page_soup = BeautifulSoup(page_html, "html.parser")
 
+ws.cell(row=1, column=1).value = "Name"
+ws.cell(row=1, column=2).value = "GD1"
+ws.cell(row=1, column=3).value = "GD2"
+ws.cell(row=1, column=4).value = "GD3"
+ws.cell(row=1, column=5).value = "AG"
 
-list = page_soup.findAll("div", attrs={"class":"inline-content"})
+list = page_soup.findAll("div", attrs={"class": "inline-content"})
 
+masterArray= []
 playerLength = 0
-for table in list:
-    tableGold = table.find_all("div", {"class": "sb-p-stat sb-p-stat-gold"})
-    tableName = table.find_all("div", {"class": "sb-p-name"})
-    for i in range(10):
-        sheet1.write(playerLength + i + 1, 0, tableName[i].text)
-        sheet1.write(playerLength + i + 1, 1, tableGold[i].text)
-    playerLength += 10
 
-wb.save("lwt example.xls")
+for table in list: # Sorts through tables
+    # Sorts through gold count per player in given table
+    tableGold = table.find_all("div", {"class": "sb-p-stat sb-p-stat-gold"})
+    # Sorts through name per player in given table
+    tableName = table.find_all("div", {"class": "sb-p-name"})
+    for i in range(10): # Max 10 players per match
+        if tableName[i].text in masterArray:
+            value_index = masterArray.index(tableName[i].text)
+            if masterArray[value_index][1]:
+                masterArray[value_index][2] = tableGold[i].text
+            else:
+                masterArray[value_index][1] = tableGold[i].text
+        else:
+            masterArray.append([tableName[i].text, tableGold[i].text])
+
+ #   playerLength += 10
+
+print(masterArray)
+#ws.cell(row=playerLength + i + 2, column=1).value = tableName[i].text
+#ws.cell(row=playerLength + i + 2, column=2).value = tableGold[i].text
+#wb.save("lwt example.xlsx")
